616e7f2dc81e6bb8c6bb45b0a270b699f468fd9f
Nataniel David ~ <43535400+IsamiYura@users.noreply.github.com>
2018-12-06 14:39:31 +0700

    finished, nggonen nyoh :v

--- Survive.java
+++ Survive.java
@@ -13,11 +13,12 @@ import java.util.*;
  */
 public class Survive {
 
-    int selector;
-
+    
     public static void main(String[] args) {
         Game_Engine mainkan = new Game_Engine();
         mainkan.menu();
+
+
     }
 
 }
